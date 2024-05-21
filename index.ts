c879e779e6928d68d8b6394725d651bfef370bfb
waseemghazigithub <159131359+waseemghazigithub@users.noreply.github.com>
2024-05-21 12:31:08 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node  
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter your frist No:", 
+  type: "number", 
+  name: "firstnumber" 
+},
+  { message: "Enter your Second No:", 
+  type: "number", 
+  name: "Secondnumber" },
+
+  {
+    message: "Select one of the operator:",
+    type: "list",
+    name: "Operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Divison"],
+  },
+]);
+
+console.log(answer);
+
+// Condition statment
+
+if (answer.Operator ==="Addition"){
+  console.log( answer.firstnumber + answer.Secondnumber)
+}
+else if 
+(answer.Operator ==="Subtraction"){
+  console.log( answer.firstnumber - answer.Secondnumber)
+}
+else if 
+(answer.Operator ==="Multiplication"){
+  console.log( answer.firstnumber * answer.Secondnumber)
+}
+else if 
+(answer.Operator ==="Divison"){
+  console.log( answer.firstnumber / answer.Secondnumber)
+}
+else {  console.log( "Please select valid operator")}
